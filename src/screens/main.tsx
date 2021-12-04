105d066e18e948602e8a481808cac74459444ff4
pedroalvarez <pedroalvarez@amalgama.co>
2021-12-04 11:34:21 -0300

    Create SVG checkmark

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,5 +1,6 @@
 import { Box, Center, Text, useColorModeValue, VStack } from 'native-base'
 import * as React from 'react'
+import AnimatedCheckbox from '../components/animated-checkbox'
 import ThemeToggle from '../components/theme-toggle'
 
 
@@ -12,6 +13,9 @@ export default function MainScreen() {
       flex={1}
     >
       <VStack space={5} alignItems="center" />
+      <Box w="100px" h="100px" >
+        <AnimatedCheckbox />
+      </Box>
       <Box p={10} bg={useColorModeValue('red.500', 'yellow.500')}>
         <Text>Hello</Text>
       </Box>
